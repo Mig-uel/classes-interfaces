388f364d61e6394f0a8c7f10b5c9555dd0cfd959
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-09-06 20:55:52 -0400

    intro to private, public members, and short hand constructor init.

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,22 @@
 "use strict";
 class Department {
-    constructor(name) {
+    constructor(name, employees = []) {
         this.name = name;
+        this.employees = employees;
     }
     describe() {
         console.log(`Department Name: ${this.name}`);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
 accounting.describe();
-const accountingCopy = { describe: accounting.describe };
+accounting.printEmployeeInformation();
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
 class Department {
-  constructor(private name: string) {}
+  constructor(private name: string, private employees: string[] = []) {}
 
-  describe(this: Department) {
+  describe(this: Department): void {
     console.log(`Department Name: ${this.name}`)
+  }
+
+  addEmployee(employee: string): void {
+    this.employees.push(employee)
+  }
+
+  printEmployeeInformation(): void {
+    console.log(this.employees.length)
+    console.log(this.employees)
   }
 }
 
 const accounting = new Department('Accounting')
-accounting.describe()
 
-const accountingCopy = { describe: accounting.describe }
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
+
+accounting.describe()
+accounting.printEmployeeInformation()
